ed0c4f670cbb56017de7268b977cb7ce2b4678da
Riccardo Nobile <riccardonobile96@gmail.com>
2017-07-04 15:39:45 +0200

    Fix a bug

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@
 let measurementsController = require('../controllers/controller.measurements');
 
 module.exports = function(router) {
-    router.get('/measurements', measurementsController.getAll)
+    router.get('/measurements', measurementsController.getAll);
     router.post('/measurements/:stationId', measurementsController.insert);
 
     return router;
